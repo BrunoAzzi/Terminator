a0444d6c74e5a758606e6d9b71e090efc55ff52b
brunoazzi <bruno.azzi@hotmail.com>
2015-09-23 17:58:08 -0300

    retrieving data from the google sheets service and maping into meta-chaordic json

--- js/main.js
+++ js/main.js
@@ -1,18 +1,81 @@
+var map = new Map();
+var product = {};
+var data = {};
+var html = [''];
+
+var ID_SELECT = "id-select";
+var URL_SELECT = "url-select";
+var CATEGORIES_SELECT = "categories-select";
+var PRICE_SELECT = "price-select";
+
 function listEvents(root) {
-  var feed = root.feed;
-  var entries = feed.entry || [];
-  var html = ['<ul>'];
+  data = root.feed.entry || [];
 
-  for (var i = 0; i < entries.length; ++i) {
-    var entry = entries[i];
-    var id = entry.term;
-
-    html.push('<li>', entry.gsx$id.$t, '</li>');
-    html.push('<li>', entry.gsx$sku.$t, '</li>');
-    html.push('<li>', entry.gsx$categories.$t, '</li>');
-    html.push('<li>', entry.gsx$status.$t, '</li>');
+  for (var property in data[0]) {
+    if(property.indexOf("gsx$") > -1){
+        html.push('<option>', property.split("$", 2)[1], '</option>');
+    }
   }
 
-  html.push('</ul>');
-  document.getElementById("agenda").innerHTML = html.join("");
+  populateSelectors(html);
+}
+
+function populateSelectors(html){
+  populateSelect(ID_SELECT, html);
+  populateSelect(URL_SELECT, html);
+  populateSelect(CATEGORIES_SELECT, html);
+  populateSelect(PRICE_SELECT, html);
+}
+
+function populateSelect(name, html){
+  document.getElementById(name).innerHTML = html.join("");
+}
+
+function generateJson(){
+  //for( i = 0; i < data.length; i++){
+   map.set("product.id","gsx$"+document.getElementById("id-select").value);
+   map.set("product.url","gsx$"+document.getElementById("url-select").value);
+   map.set("product.categories","gsx$"+document.getElementById("categories-select").value);
+   map.set("product.price","gsx$"+document.getElementById("price-select").value);
+
+   populateId(searchData(0,map.get("product.id")));
+   populateUrl(searchData(0,map.get("product.url")));
+   product.categories = searchData(0,map.get("product.categories")).split("-");
+   populatePrice(searchData(0,map.get("product.price")));
+
+ //}
+   console.log(JSON.stringify(product));
+}
+
+function populateId(value){
+  if(value != null && value != ""){
+    product.id = value;
+  }
+}
+
+function populateUrl(value){
+  if(value != null && value != ""){
+    product.url = value;
+  }
+}
+
+function populateCategories(value){
+  if(value != null && value != ""){
+    categoryList = value.split(">");
+    //TODO implement
+  }
+}
+
+function populatePrice(value){
+  if(value != null && value != ""){
+    product.price = value;
+  }
+}
+
+function searchData(linha, coluna){
+  for(var columnName in data[linha]){
+    if(columnName.indexOf(coluna) > -1){
+      return data[linha][columnName].$t;
+    }
+  }
 }
